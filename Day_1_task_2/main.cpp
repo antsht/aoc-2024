46578951a644258e2bac2f66e7402d991a7335b3
Anton <antsht@gmail.com>
2024-12-01 14:10:30 +0300

    More refactoring...

--- Day_1_task_2/main.cpp
+++ Day_1_task_2/main.cpp
@@ -2,24 +2,25 @@
 #include <functional>
 #include <iostream>
 #include <unordered_map>
+#include <cstdint>
 
-void process_input_file(const char* fileName,
-                        std::function<void(int, int)> processingFunction);
+template <typename Func>
+void process_input_file(const char* fileName, Func processingFunction);
 
 int main() {
   std::unordered_map<unsigned int, unsigned int> data;
 
-  process_input_file("input", [&data](int left, [[maybe_unused]] int right) {
-    data[left];
-  });
+  process_input_file(
+      "input", [&data](int left, [[maybe_unused]] int right) { data[left]; });
 
   process_input_file("input", [&data]([[maybe_unused]] int left, int right) {
-    if (data.contains(right)) {
-      data[right]++;
+    auto it = data.find(right);
+    if (it != data.end()) {
+      it->second++;
     }
   });
 
-  u_int64_t result = 0;
+  std::uint64_t result = 0;
   for (const auto& [key, value] : data) {
     result += key * value;
   }
@@ -28,17 +29,20 @@ int main() {
   return 0;
 }
 
-void process_input_file(const char* fileName,
-                        std::function<void(int, int)> processingFunction) {
+template <typename Func>
+void process_input_file(const char* fileName, Func processingFunction) {
   {
     std::ifstream inputFS(fileName);
-    if (inputFS.is_open()) {
-      int left, right;
-      while (inputFS >> left >> right) {
-        processingFunction(left, right);
-      }
-    } else {
-      throw std::runtime_error("Error: Unable to open input file.");
+    if (!inputFS.is_open()) {
+      throw std::runtime_error("Error: Unable to open input file." +
+                               std::string(fileName));
+    }
+    int left, right;
+    while (inputFS >> left >> right) {
+      processingFunction(left, right);
+    }
+    if (!inputFS.eof() && inputFS.fail()) {
+      throw std::runtime_error("Error: Corrupted or invalid file format.");
     }
   }
 }
