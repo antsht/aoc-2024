345fa0ce5166d291ba85ed50ce92f3410e3d255d
Anton Shtern <antsht@gmail.com>
2024-12-02 10:37:00 +0300

    Refactoring...

--- Day_2_task_2/main.cpp
+++ Day_2_task_2/main.cpp
@@ -2,47 +2,40 @@
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <sstream>
 #include <vector>
 
-bool isSafe(std::vector<int>& numbers);
+bool isSafe(const std::vector<int> &numbers);
 
 int main() {
   std::uint32_t result = 0;
-  {
-    std::ifstream inputFS("./input");
 
-    if (!inputFS.is_open()) {
-      std::cerr << "Error: Unable to open input file." << std::endl;
-      return 1;
+  std::ifstream inputFS("./input");
+
+  if (!inputFS.is_open()) {
+    std::cerr << "Error: Unable to open input file." << std::endl;
+    return 1;
+  }
+
+  std::string line;
+  while (std::getline(inputFS, line)) {
+    std::istringstream lineStream(line);
+    std::vector<int> numbers{std::istream_iterator<int>(lineStream),
+                             std::istream_iterator<int>()};
+
+    if (isSafe(numbers)) {
+      ++result;
+      continue;
     }
 
-    std::string line;
-    while (std::getline(inputFS, line)) {
-      std::istringstream lineStream(line);
-      std::vector<int> numbers;
-      int num;
-
-      while (lineStream >> num) {
-        numbers.push_back(num);
-      }
-      
-      if (isSafe(numbers)) {
+    for (size_t i = 0; i < numbers.size(); ++i) {
+      std::vector<int> newNumbers = numbers;
+      newNumbers.erase(newNumbers.begin() + i);
+      if (isSafe(newNumbers)) {
         ++result;
-        continue;
+        break;
       }
-
-      for (size_t i = 0; i < numbers.size(); ++i)
-      {
-        std::vector<int> newNumbers = numbers;
-        newNumbers.erase(newNumbers.begin() + i);
-        if (isSafe(newNumbers)) {
-          ++result;
-          break;
-        }
-      }
-      
-
     }
   }
 
@@ -51,20 +44,18 @@ int main() {
   return 0;
 }
 
-
-bool isSafe(std::vector<int>& numbers){
+bool isSafe(const std::vector<int> &numbers) {
+  if (numbers.size() < 2) {
+    return true;
+  }
   bool ascOrder = (numbers[0] < numbers[1]);
   bool isSafe = true;
-      for (size_t i = 0; isSafe && (i < (numbers.size()-1)); ++i) {
-        
-        if (ascOrder && !(((numbers[i + 1] - numbers[i]) >= 1) &&
-                          ((numbers[i + 1] - numbers[i]) <= 3))) {
-            isSafe = false;
-        }
-        if (!ascOrder && !(((numbers[i] - numbers[i+1]) >= 1) &&
-                          ((numbers[i] - numbers[i+1]) <= 3))) {
-            isSafe = false;
-        }
-      }
+  for (size_t i = 0; i < numbers.size() - 1; ++i) {
+    int diff =
+        ascOrder ? numbers[i + 1] - numbers[i] : numbers[i] - numbers[i + 1];
+    if (diff < 1 || diff > 3) {
+      return false;
+    }
+  }
   return isSafe;
 }
