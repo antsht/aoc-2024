2428c792035b43753b428f2edcc4b8bb2d2b0e21
Anton Shtern <antsht@gmail.com>
2024-12-02 09:13:20 +0300

    Day 2 task 1

--- Day_2_task_1/main.cpp
+++ Day_2_task_1/main.cpp
@@ -24,23 +24,22 @@ int main() {
       while (lineStream >> num) {
         numbers.push_back(num);
       }
-
-      std::cout << "Processed Line: ";
-      bool ascOrder = numbers[0] < numbers[1];
+      bool ascOrder = (numbers[0] < numbers[1]);
       bool isSafe = true;
-      for (size_t i = 0; i < numbers.size()-1; ++i) {
-        if (ascOrder && !((numbers[i + 1] - numbers[i]) >= 1 &&
-                          (numbers[i + 1] - numbers[i]) <= 3)) {
+      for (size_t i = 0; isSafe && (i < (numbers.size()-1)); ++i) {
+        
+        if (ascOrder && !(((numbers[i + 1] - numbers[i]) >= 1) &&
+                          ((numbers[i + 1] - numbers[i]) <= 3))) {
             isSafe = false;
-            break;
         }
-        if (!ascOrder && !((numbers[i] - numbers[i+1]) >= 1 &&
-                          (numbers[i] - numbers[i+1]) <= 3)) {
+        if (!ascOrder && !(((numbers[i] - numbers[i+1]) >= 1) &&
+                          ((numbers[i] - numbers[i+1]) <= 3))) {
             isSafe = false;
-            break;
         }
       }
-      std::cout << std::endl;
+      if (isSafe) {
+        ++result;
+      }
     }
   }
 
