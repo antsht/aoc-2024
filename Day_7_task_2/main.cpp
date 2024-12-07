6af33068ce9f34cac7bc57ef47d90e14ac8bbc4e
Anton <antsht@gmail.com>
2024-12-07 13:47:17 +0300

    Day 7 Task 2

---
+++ Day_7_task_2/main.cpp
@@ -0,0 +1,79 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using valueType = std::uint64_t;
+valueType checkLine(const std::vector<valueType> &line);
+
+valueType concatenateNumbers(valueType num1, valueType num2);
+
+int main() {
+  std::ifstream inputFS("./input");
+  if (!inputFS.is_open()) {
+    std::cerr << "Error: Unable to open input file." << std::endl;
+    return 1;
+  }
+  std::vector<std::vector<valueType>> inputData;
+  inputData.reserve(850);
+  std::string line;
+  while (std::getline(inputFS, line)) {
+    std::stringstream lineStream(line);
+    std::vector<valueType> lineToCheck;
+    valueType num;
+    char c;
+    lineStream >> num >> c;
+    lineToCheck.push_back(num);
+    while (lineStream >> num) {
+      lineToCheck.push_back(num);
+    }
+    inputData.push_back(lineToCheck);
+  }
+  inputFS.close();
+
+  // print inputData
+  for (const auto &line : inputData) {
+    for (const auto &num : line) {
+      std::cout << num << " ";
+    }
+    std::cout << std::endl;
+  }
+  valueType result = 0;
+  for (const auto &line : inputData) {
+      result+=checkLine(line);
+  }
+  std::cout << result << std::endl;
+      return 0;
+  }
+
+  valueType checkLine(const std::vector<valueType> &line) {
+      valueType result = line[0];
+      std::vector<valueType> resultV1;
+      resultV1.push_back(line[1]);
+      std::vector<valueType> resultV2;
+      for (size_t i = 2; i < line.size(); ++i) {
+      for (auto &num : resultV1) {
+      resultV2.push_back(num + line[i]);
+      resultV2.push_back(num * line[i]);
+      resultV2.push_back(concatenateNumbers(num, line[i]));
+    }
+    std::swap(resultV1, resultV2);
+    resultV2.clear();
+  }
+  for (auto &num : resultV1) {
+    if (num == result) {
+      return result;
+    }
+  }
+  return 0;
+  }
+
+  valueType concatenateNumbers(valueType num1, valueType num2) {
+  std::stringstream ss;
+  ss << num1 << num2;
+  valueType result;
+  ss >> result;
+  return result;
+  }
