8db971b01e7e54a00f8b2d6d9c16cef4142f3fe1
Anton <antsht@gmail.com>
2024-12-11 14:32:11 +0300

    Day 11

--- Day_11_task_1/main.cpp
+++ Day_11_task_1/main.cpp
@@ -1,17 +1,18 @@
+#include <cmath>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <sstream>
 #include <string>
-#include <list>
 
 using valueType = std::uint64_t;
 void printData(const std::list<valueType> &data);
-void processData(const std::list<valueType> &data);
-
+void processData(std::list<valueType> &data);
+size_t countDigits(valueType value);
 
 int main() {
-  std::ifstream inputFS("./input_test");
+  std::ifstream inputFS("./input");
   if (!inputFS.is_open()) {
     std::cerr << "Error: Unable to open input file." << std::endl;
     return 1;
@@ -20,25 +21,45 @@ int main() {
   valueType currentValue;
 
   while (inputFS >> currentValue) {
-        data.push_back(currentValue);
+    data.push_back(currentValue);
   }
   inputFS.close();
   printData(data);
+  for (size_t i = 0; i < 25; i++) {
+    processData(data);
+  }
+    
+    std::cout << "Result: " << data.size() << std::endl;
 
   return 0;
 }
 
 void printData(const std::list<valueType> &data) {
   for (auto i : data) {
-      std::cout << i << " ";
+    std::cout << i << " ";
   }
   std::cout << std::endl;
 }
 
-void processData(const std::list<valueType> &data) {
-  for (auto i : data) {
-      if (i==0){i=1;} else if (){} else {i*=2024;}
+void processData(std::list<valueType> &data) {
+  for (auto it = data.begin(); it != data.end(); ++it) {
+    if (*it == 0) {
+      *it = 1;
+    } else if (size_t digits = countDigits(*it); digits % 2 == 0) {
+      data.insert(it, (*it) / std::pow(10, digits / 2));
+      *it = *it % static_cast<int>((std::pow(10, digits / 2)));
+
+    } else {
+      *it *= 2024;
+    }
   }
-  std::cout << std::endl;
 }
 
+size_t countDigits(valueType value) {
+  size_t count = 0;
+  while (value > 0) {
+    value /= 10;
+    count++;
+  }
+  return count;
+}
